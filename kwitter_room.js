0a9e7ad185478a5baeb3124c9468afe310cdd0b8
JonahMasilang <89702146+JonahMasilang@users.noreply.github.com>
2022-04-14 21:03:42 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9y4He7DsRxO_Znps9hcVfkHft_A6_Jik",
+    authDomain: "lets-chat-web-app-a97b9.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-a97b9-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-a97b9",
+    storageBucket: "lets-chat-web-app-a97b9.appspot.com",
+    messagingSenderId: "585343316030",
+    appId: "1:585343316030:web:4a1c24ed93b134f042e16f",
+    measurementId: "G-QWNM7D90WG"
+  };
+
+  firebase.initializeApp(config);
